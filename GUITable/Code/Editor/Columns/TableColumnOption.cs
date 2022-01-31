aebe4bcf62ec7113f7d71d27d6cfba5855a6bdad
이성주 <sjlee@eggtartsoft.com>
2022-01-31 14:31:57 +0900

    Imported GUITable

---
+++ GUITable/Code/Editor/Columns/TableColumnOption.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EditorGUITable
+{
+
+	public class TableColumnOption
+	{
+
+		public enum Type
+		{
+			ExpandWidth,
+			Width,
+			MinWidth,
+			MaxWidth,
+			Resizeable,
+			Sortable,
+			EnabledCells,
+			EnabledTitle,
+			Optional,
+			VisibleByDefault,
+		}
+
+		public Type type;
+		public object value;
+
+		public TableColumnOption(Type type, object value)
+		{
+			this.type = type;
+			this.value = value;
+		}
+	}
+
+}
