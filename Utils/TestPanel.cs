e023553ce7caa0c6c4fafec97ff8eb9d9620115f
sando87 <tjdwn0406@gmail.com>
2022-11-10 11:19:33 +0900

    TestPanel 기능 추가

--- Utils/TestPanel.cs
+++ Utils/TestPanel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 
 /// <summary>
@@ -8,6 +9,19 @@ using UnityEngine;
 
 public class TestPanel : MonoBehaviour
 {
+    [SerializeField] int _WaveNumber = 1;
+    [SerializeField] int _Level = 1;
+    [Dropdown("GetUnitIDs")]
+    public long UnitID;
+    private DropdownList<long> GetUnitIDs()
+    {
+        DropdownList<long> units = new DropdownList<long>();
+        foreach(var unit in UserCharactors.Inst.Enums())
+            units.Add(unit.name, unit.ID);
+
+        return units;
+    }
+
     private bool mIsShow = true;
     private GUIStyle mGuiStyle = null;
 
@@ -15,6 +29,7 @@ public class TestPanel : MonoBehaviour
     {
         DontDestroyOnLoad(this.gameObject);
     }
+    
 
     void Update()
     {
@@ -25,36 +40,62 @@ public class TestPanel : MonoBehaviour
         }
     }
 
-    void OnGUI()
+    [Button]
+    void CreateNewUnit()
     {
-        if(!mIsShow) return;
-
-        if(mGuiStyle == null)
-        {
-            mGuiStyle = new GUIStyle(GUI.skin.button);
-            mGuiStyle.fontSize = 30;
-        }
-
-        GUILayout.BeginVertical();
-
-        GUILayout.Button("'='키 눌러서 test패널 On/Off", mGuiStyle);
-
-        if (GUILayout.Button("게임 데이터 초기화", mGuiStyle))
-        {
-        }
-
-        if (GUILayout.Button("기능 1", mGuiStyle))
-        {
-        }
-
-        if (GUILayout.Button("기능 2", mGuiStyle))
-        {
-        }
-
-        if (GUILayout.Button("InGame 성공처리", mGuiStyle))
-        {
-        }
-
-        GUILayout.EndVertical();
+        BaseObject newUnit = InGameSystem.Instance.CreateUnit(UnitID);
+        newUnit.SpecProp.Level = _Level;
     }
+    
+    [Button]
+    void MoveToWave()
+    {
+        InGameSystem.Instance.NextWaveNumberForTest = _WaveNumber;
+    }
+    [Button]
+    void EarnKillPoint()
+    {
+        InGameSystem.Instance.KillPoint += 10;
+    }
+    [Button]
+    void EarnMineral()
+    {
+        InGameSystem.Instance.Mineral += 100;
+    }
+
+    // void OnGUI()
+    // {
+    //     if(!mIsShow) return;
+
+    //     if(mGuiStyle == null)
+    //     {
+    //         mGuiStyle = new GUIStyle(GUI.skin.button);
+    //         mGuiStyle.fontSize = 30;
+    //     }
+
+    //     GUILayout.BeginVertical();
+
+    //     if (GUILayout.Button("'='키 눌러서 test패널 On/Off", mGuiStyle))
+    //     {
+    //         mIsShow = !mIsShow;
+    //     }
+
+    //     if (GUILayout.Button("게임 데이터 초기화", mGuiStyle))
+    //     {
+    //     }
+
+    //     if (GUILayout.Button("기능 1", mGuiStyle))
+    //     {
+    //     }
+
+    //     if (GUILayout.Button("기능 2", mGuiStyle))
+    //     {
+    //     }
+
+    //     if (GUILayout.Button("InGame 성공처리", mGuiStyle))
+    //     {
+    //     }
+
+    //     GUILayout.EndVertical();
+    // }
 }
